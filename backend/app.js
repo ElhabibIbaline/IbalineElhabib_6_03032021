a59c3697cbc35f63054307512650a43ef54940be
IbalineElhabib <h.ibaline@gmail.com>
2022-02-19 20:57:12 +0100

    Créer des routes pour les  utilisateurs

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+// enregistrer le router dans l'application 
+const userRoutes = require('./routes/user');
+
 mongoose
   .connect(
     "mongodb+srv://ElhabibP6:oXuXTrXu03piHihO@cluster0.kzycm.mongodb.net/DevWebP6?retryWrites=true&w=majority",
@@ -12,23 +15,14 @@ mongoose
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+//Bodyparser est inclus ds express à partir de la version 4.16 ==> Utilisez ( express.json() ) pour analyser le corps de la requête.
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.json({ message: "Votre requête a bien été reçue !" });
-  next();
 });
 
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
+// Enreistrer les routes ==>la route attendu par le frontend est '/api/auth'
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ backend/routes/user.js
@@ -0,0 +1,21 @@
+
+//créer le router correspondnat
+
+//Importer express pr créer un Router
+const express = require('express');
+
+//Créer le Router avec la f Router d'express
+const router = express.Router();
+
+// importer le controller pr associer les f aux differentes routes
+const userCtrl = require('../controllers/user');
+
+
+// Créer 2 routes post ==> Car le frontend va également envoyer des infos ==> l'adresse mail et le MP
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
+//Exporter le router pour l'importer ds app.js
+
+module.exports = router;
+
