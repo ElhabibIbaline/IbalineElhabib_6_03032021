1f16fb7f56ee75b53b8cf6d257a24f34b6fa961e
IbalineElhabib <h.ibaline@gmail.com>
2022-02-20 08:15:32 +0100

    Implimenter la fonction SIGNUP pour enregistrer les utilisateurs

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,13 @@
 const express = require("express");
 
-// enregistrer le router dans l'application 
-const userRoutes = require('./routes/user');
+// enregistrer le router dans l'application
+const userRoutes = require("./routes/user");
 
-const mongoose = require('./db/db');
+const mongoose = require("./db/db");
 
+const app = express();
 
-
-// Connection à la base de données MongoDB avec la sécurité vers le fichier .env pour cacher le mot de passe
-// L'un des avantages que nous avons à utiliser Mongoose pour gérer notre base de données MongoDB est que nous pouvons implémenter des schémas de données stricts
-// qui permettent de rendre notre application plus robuste
-// mongoose.connect(process.env.DB_URI, {
-//   useCreateIndex: true,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-// .then(() => console.log('Connexion à MongoDB réussie !'))
-// .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-
-  const app = express();
-
-  //Bodyparser est inclus ds express à partir de la version 4.16 ==> Utilisez ( express.json() ) pour analyser le corps de la requête.
+//Bodyparser est inclus ds express à partir de la version 4.16 ==> Utilisez ( express.json() ) pour analyser le corps de la requête.
 app.use(express.json());
 
 // --2--cros
@@ -38,12 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
-
-// app.use((req, res, next) => {
-//   res.json({ message: "Votre requête a bien été reçue !" });
-// });
-
 // Enreistrer les routes ==>la route attendu par le frontend est '/api/auth'
-app.use('/api/auth', userRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,28 @@
 // Créer les routes d'authentification
 
-//// La f login pour l'inscription des utilisateurs
+// Importer le package de cryptage pr les mp
+const bcrypt = require("bcrypt");
+
+// Importer model User car on va enregistrer et lire des users ds ce mdlw
+const User = require("../models/User");
+
+// La f signup pour l'enregistrement des nouveaux utilsateurs
 exports.signup = (req, res, next) => {
- 
+  // Hasher le mp et créer un new user avec le mp crypté...
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      }); // Enregistrer le hash le mp crypté ds la bdd //--On utilise la methode save pr l'enregistrer ds la BDD
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
-
 // La f login pour connecter les utilisateurs existants
-exports.login = (req, res, next) => {
-   
-  };
+exports.login = (req, res, next) => {};
