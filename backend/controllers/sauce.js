e7516481f724f1a56260e800a6238840676d79e6
IbalineElhabib <h.ibaline@gmail.com>
2022-03-15 20:09:48 +0100

    Modifier et mettre à jour une sauce existante

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -17,6 +17,18 @@ exports.createSauce = (req, res, next) => {
     .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
     .catch(error => res.status(400).json({ error }));
 };
+
+exports.updateSauce = (req, res, next) => {
+  const sauceObject = req.file ?
+  {
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`} : { ...req.body }
+  Sauce.updateOne({ _id : req.params.id}, {...sauceObject, _id: req.params.id})
+  
+  .then(res.status(200).json({ message : "Sauce modifiée"}))
+  .catch(error => res.status(400).json({ error }))
+}
+
 // Lire et afficher une seule sauce de la bdd.
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,24 +1,14 @@
-
-//Importer express pr créer un Router
 const express = require('express');
-
-//Créer le Router avec la f Router d'express
 const router = express.Router();
 
-// importer le controller pr associer les f aux differentes routes
 const sauceCtrl = require('../controllers/sauce.js');
-
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config')
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
-
-// Renvoie la sauce avec l’_id fourni, affiche une seule sauce.
+router.put("/:id", auth, multer, sauceCtrl.updateSauce)
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-
-// // Renvoie un tableau de toutes les sauces de la base de données.
 router.get("/", auth, sauceCtrl.getAllSauces);
 
-//Exporter le router pour l'importer ds app.js
 module.exports = router;
 
