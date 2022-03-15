c7455de47aaf2a47b057b21dc12e362316d892c8
IbalineElhabib <h.ibaline@gmail.com>
2022-03-15 22:14:33 +0100

    Completer le CRUD / Ajouter la route delete et importer le package fs

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const sauceCtrl = require('../controllers/sauce.js');
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config')
+const sauceCtrl = require("../controllers/sauce.js");
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-router.post('/', auth, multer, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, sauceCtrl.updateSauce)
-router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.put("/:id", auth, multer, sauceCtrl.updateSauce);
+router.delete("/:id", auth, multer, sauceCtrl.deleteSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.get("/", auth, sauceCtrl.getAllSauces);
 
 module.exports = router;
-
