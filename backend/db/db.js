dccbea82991ee1596713f99c7b0ad280931c2cfe
elhabib ibaline <h.ibaline@gmail.com>
2024-11-28 06:34:07 +0100

    update dependencies and replace node-sass with sass

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+mongoose.set("strictQuery", true); // Supprime l'avertissement de dépréciation
+
 mongoose
   .connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"));
+  .catch((err) => {
+    console.error("Erreur de connexion à MongoDB :", err.message);
+    process.exit(1); // Arrête le processus en cas d'échec
+  });
 
 module.exports = mongoose;
